4be130f5f087b3493a5f7d5c55cdb83435351f1f
Andrey Burdak <burdak1994@gmail.com>
2026-07-06 18:40:17 +0300

    Выполнить задание 3. Используем базовые аннотации

--- src/main/java/ru/practicum/item/Item.java
+++ src/main/java/ru/practicum/item/Item.java
@@ -1,10 +1,21 @@
 package ru.practicum.item;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "items")
+
 public class Item {
+    @Id
     private Long id;
+    @Column(name = "user_id")
     private Long userId;
+    @Column(name = "url")
     private String url;
+
 }
--- src/main/java/ru/practicum/user/User.java
+++ src/main/java/ru/practicum/user/User.java
@@ -1,12 +1,29 @@
 package ru.practicum.user;
 
+import jakarta.persistence.*;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+
+import java.time.Instant;
 
 @Data
-@NoArgsConstructor
+@Entity
+@Table(name = "users", schema = "public")
+
 public class User {
+    @Id
     private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
     private String email;
-    private String name;
+
+    @Column(name = "registration_date")
+    private Instant registrationDate = Instant.now();
+
+    @Enumerated(value = EnumType.STRING)
+    private UserState state;
 }
