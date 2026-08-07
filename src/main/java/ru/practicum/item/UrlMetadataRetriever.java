5516ecbcb39ca17b5cb75985fdc883f1d2c279ca
Andrey Burdak <burdak1994@gmail.com>
2026-08-07 18:55:12 +0300

    refactor: improve UrlMetadataRetrieverImpl structure and enhance metadata handling

--- src/main/java/ru/practicum/item/UrlMetadataRetriever.java
+++ src/main/java/ru/practicum/item/UrlMetadataRetriever.java
@@ -2,25 +2,23 @@ package ru.practicum.item;
 
 import java.time.Instant;
 
-public interface UrlMetadataRetriever {
+interface UrlMetaDataRetriever {
+    UrlMetadata retrieve(String uri);
 
-    UrlMetadata retrieve(String url);
-
+    // Закрепляем в виде вложенного интерфейса контракт
+    // об обязательных данных, которые должны возвращать
+    // все реализации UrlMetaDataRetriever'а.
+    // Отдельные реализации могут дополнять метаданные
+    // новой информацией (расширяя своими интерфейсами
+    // или сразу добавляя в свои реализации UrlMetadata),
+    // но описанное здесь, должно быть у всех реализаций.
     interface UrlMetadata {
-
-        Instant getDateResolver();
-
-        String getMimeType();
-
         String getNormalUrl();
-
         String getResolvedUrl();
-
+        String getMimeType();
         String getTitle();
-
         boolean isHasImage();
-
         boolean isHasVideo();
-
+        Instant getDateResolved();
     }
 }
